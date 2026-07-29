cd455d2b6c8d8c3f8e4260bf25030de28b12eb3c
MadhuVarshini <madhuvarshini2604@gmail.com>
2026-07-29 20:49:11 +0530

    Update views.py

--- books/views.py
+++ books/views.py
@@ -1,9 +1,4 @@
 from django.shortcuts import render
-from .models import Book
 
 def book_list(request):
-    books = Book.objects.all()
-
-    return render(request, 'books/book_list.html', {
-        'books': books
-    })
+    return render(request, "book_list.html")
