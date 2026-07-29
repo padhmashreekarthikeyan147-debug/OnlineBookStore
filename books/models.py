f36df82ec4039df107937f50aeb138b8ca08866a
MadhuVarshini <madhuvarshini2604@gmail.com>
2026-07-29 20:47:44 +0530

    Update models.py

--- books/models.py
+++ books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    image = models.ImageField(upload_to='books/', blank=True, null=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
