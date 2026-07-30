d5da31c5a8cc3c2c1c5df9df96198e6061186bbc
madhuvarshini2604-dot <madhuvarshini2604@gmail.com>
2026-07-30 22:01:46 +0530

    Completed Day 1 - Database Model

--- books/models.py
+++ books/models.py
@@ -1,11 +1,31 @@
 from django.db import models
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=20, unique=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    image = models.ImageField(upload_to='books/', blank=True, null=True)
+    stock = models.PositiveIntegerField()
     description = models.TextField()
+    cover_image = models.ImageField(upload_to='books/', blank=True, null=True)
+    published_date = models.DateField()
 
     def __str__(self):
         return self.title
