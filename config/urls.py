610df1a7faa66ffe237d4db31331c23fff817f5b
Harini Ganesan <gharini558@gmail.com>
2026-07-30 23:49:20 +0530

    updated Newsletter app files

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('books/', views.book_list, name='book_list'),
+    path("admin/", admin.site.urls),
+    path("newsletter/", include("newsletter.urls")),
 ]
--- newsletter/models.py
+++ newsletter/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+class Subscriber(models.Model):
+    email = models.EmailField(unique=True)
+    subscribed_at = models.DateTimeField(auto_now_add=True)
 
-# Create your models here.
+    def _str_(self):
+        return self.email
